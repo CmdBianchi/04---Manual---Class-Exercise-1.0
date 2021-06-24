426b82ac88cdef5fe69279d5ec06ef36dd24419b
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-24 15:38:45 -0300

    Adicionar arquivos de projeto.

---
+++ 04 - Manual - Class Exercise 1.0/Entities/Stock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Stock {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        public Stock() { }
+        public Stock(string name, double price, int amount) {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+        public double TotalInSotock() {
+            return Amount * Price;
+        }
+        public void AddProduct(int amount) {
+            Amount += amount;
+        }
+        public void RemoveProduct(int amount) {
+            Amount -= amount;
+        }
+
+        public override string ToString() {
+            return Name + ", $" + Price.ToString("F2") + ", " + Amount + " valor das unidades no estoque, TOTAL: $" + TotalInSotock().ToString("F2");
+
+        }
+    }
+}
---
+++ 04 - Manual - Class Exercise 1.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Entities;
+namespace _04___Manual___Class_Exercise_1._0 {
+    class Program {
+        static void Main(string[] args) {
+            Stock stock = new Stock();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Entre com os valores dos dados do produto");
+            Console.Write("Nome do produto: "); stock.Name = Console.ReadLine();
+            Console.Write("Preço do produto: "); stock.Price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: "); stock.Amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine(stock);
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("");
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: "); int add = int.Parse(Console.ReadLine());
+            stock.AddProduct(add);
+            Console.Write("Digite o número de produtos a ser removido do estoque: "); int remove = int.Parse(Console.ReadLine());
+            stock.RemoveProduct(remove);
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine(stock);
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------------------------");
+        }
+    }
+}
+
